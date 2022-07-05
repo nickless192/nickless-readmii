8b14bfd6c4890653869887063f0b4558128f1a21
Omar Rodriguez <omar.rguez26@gmail.com>
2022-07-05 17:11:42 -0400

    set up npm modules

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const {renderLicenseBadge, renderLicenseLink, renderLicenseSection, generateMarkdown} = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
